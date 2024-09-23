fe28e2b2f79eac292468e014c8516fed649e8cf0
danxham <notrackersonme@duck.com>
2024-09-23 10:37:04 -0400

    black screen working, added a git.ignore

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 import pygame
 from constants import *
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
-    pygame.Surface.fill(screen, 000)
-    pygame.display.flip()
+
 
 
 def main():
@@ -14,6 +9,12 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+    pygame.Surface.fill(screen, 000)
+    pygame.display.flip()
 
 if __name__ == "__main__":
     main()
