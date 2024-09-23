860403cec3cb68917bac08a9ae1918489a164d9f
danxham <notrackersonme@duck.com>
2024-09-23 10:50:38 -0400

    added fps limit

--- main.py
+++ main.py
@@ -6,15 +6,22 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
+    
     pygame.Surface.fill(screen, 000)
     pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
